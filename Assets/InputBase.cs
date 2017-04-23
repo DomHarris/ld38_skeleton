cb7d347175a575e1e8373517ec7703ee9ecd9f50
Dom Harris <dom@appt.so>
2017-04-24 00:25:37 +0100

    Initial upload

---
+++ Assets/InputBase.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public enum PlayerEnum {
+	p1_,
+	p2_,
+	p3_,
+	p4_
+}
+
+public abstract class InputBase : MonoBehaviour
+{
+	protected bool isActive = true;
+	[SerializeField] protected PlayerEnum player = PlayerEnum.p1_;
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(GetInputs());
+	}
+	
+	// Update is called once per frame
+	IEnumerator GetInputs ()
+	{
+		while (isActive)
+		{
+			LeftHorizontal(Input.GetAxis(player + "LeftHorizontal"));
+		
+			LeftVertical(Input.GetAxis(player + "LeftVertical"));
+		
+			RightHorizontal(Input.GetAxis(player + "RightHorizontal"));
+		
+			RightVertical(Input.GetAxis(player + "RightVertical"));
+			
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	protected abstract void LeftHorizontal (float val);
+	protected abstract void LeftVertical (float val);
+	protected abstract void RightHorizontal (float val);
+	protected abstract void RightVertical (float val);
+}
---
+++ Assets/InputHandler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class InputHandler : InputBase
+{
+	[SerializeField] float speed = 5;
+	[SerializeField] ParticleSystem ps; 
+
+	float rightX = 0;
+	float rightY = 0;
+	protected override void LeftHorizontal (float val)
+	{
+		transform.position += Vector3.right * val * Time.deltaTime * speed;
+	}
+	protected override void LeftVertical (float val)
+	{
+		transform.position += Vector3.up * val * Time.deltaTime * speed;
+	}
+	protected override void RightHorizontal (float val)
+	{
+		rightX = val;
+	}
+	protected override void RightVertical (float val)
+	{
+		rightY = val;
+	}
+
+
+	void Update ()
+	{
+		if (rightX != 0 || rightY != 0)
+		{
+			ParticleSystem.EmitParams ep = new ParticleSystem.EmitParams();
+			ep.velocity = new Vector3 (rightX, rightY) * 8;
+			ep.position = transform.position;
+			ep.startSize = 0.5f;
+			ps.Emit(ep, 1);
+
+		}
+	}
+}
